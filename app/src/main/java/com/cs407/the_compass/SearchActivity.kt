ee47746c845bcacbc361a9a0acd5a0d63d3d998b
zz2556 <zzhang2556@wisc.edu>
2024-11-17 13:48:22 -0600

    adding button function and temp searching toast for search button

--- app/src/main/java/com/cs407/the_compass/SearchActivity.kt
+++ app/src/main/java/com/cs407/the_compass/SearchActivity.kt
@@ -1,6 +1,10 @@
 package com.cs407.the_compass
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,20 @@ class SearchActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val returnBtn = findViewById<ImageView>(R.id.btnReturn)
+        val doSearchBtn = findViewById<ImageView>(R.id.btnConfirmSearch)
+        val searchBar = findViewById<EditText>(R.id.searchText)
+
+        returnBtn.setOnClickListener {
+            val intent = Intent(this, NavigationActivity::class.java)
+            startActivity(intent)
+        }
+
+        doSearchBtn.setOnClickListener {
+            val userInputText = searchBar.text.toString()
+            Toast.makeText(this, "Searching: "+userInputText, Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
