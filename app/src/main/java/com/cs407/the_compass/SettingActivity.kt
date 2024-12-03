2f92c73040a09e1c9d3c9b560b7c0c5b6fb5b2e8
zz2556 <zzhang2556@wisc.edu>
2024-12-02 18:11:55 -0600

    add on/off for location log switcher

--- app/src/main/java/com/cs407/the_compass/SettingActivity.kt
+++ app/src/main/java/com/cs407/the_compass/SettingActivity.kt
@@ -3,6 +3,7 @@ package com.cs407.the_compass
 import android.os.Bundle
 import android.content.Intent
 import android.widget.ImageView
+import android.widget.Switch
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +18,8 @@ class SettingActivity : AppCompatActivity() {
 
         val btnHome = findViewById<ImageView>(R.id.BtnReturn_Set)
         //val btnMap = findViewById<ImageView>(R.id.btnMap)
+        val locationLogSwitch = findViewById<Switch>(R.id.locationLogSwitch)
+        val logStatusText = findViewById<TextView>(R.id.LogstatusText)
 
         btnHome.setOnClickListener {
 //            val intent = Intent(this, MainActivity::class.java)
@@ -24,6 +27,11 @@ class SettingActivity : AppCompatActivity() {
             finish()
         }
 
+        logStatusText.text = if (locationLogSwitch.isChecked) "On" else "Off"
+        // Add a listener to the Switch
+        locationLogSwitch.setOnCheckedChangeListener { _, isChecked ->
+            logStatusText.text = if (isChecked) "On" else "Off"
+        }
         /**btnMap.setOnClickListener{
             val intent = Intent(this,NavigationActivity::class.java)
             startActivity(intent)
