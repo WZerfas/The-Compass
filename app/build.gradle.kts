a4cf64d15ce820953a7f3ca997a2e1a2ca96fe62
the-snowmen <hdb3837@gmail.com>
2024-12-04 00:47:12 -0600

    Fix the search bar display inconvenience

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.cs407.the_compass"
         minSdk = 24
         targetSdk = 34
-        versionCode = 16
-        versionName = "1.5.2"
+        versionCode = 17
+        versionName = "1.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
